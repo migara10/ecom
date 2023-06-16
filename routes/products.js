c88fc3adcd0fcb9b04748711c9d354e87ee8dbdb
migaa10 <migaraten@gmail.com>
2023-06-16 18:04:25 +0530

    add product

--- routes/products.js
+++ routes/products.js
@@ -6,10 +6,11 @@ const path = require('path')
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: './upload/images',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    filename: function (req, file, cb) {
+      return cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
     }
-})
+  });
+  
 const upload = multer({
     storage: storage
 })

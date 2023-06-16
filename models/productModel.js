05c6d455672505e7f5b4af849be6042e9579a6a3
migaa10 <migaraten@gmail.com>
2023-06-16 16:34:57 +0530

    fix changes

---
+++ models/productModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const productSchema = new Schema({
+
+    product_id: {
+        type: Number,
+    },
+    product_name: {
+        type: String,
+        required: true, 
+    },
+    product_category: {
+        type: String,
+        required: true, 
+    },
+    product_disc: {
+        type: String,
+        required: false, 
+    },
+    product_imageuri:{
+        type: String,
+        required: true,
+    },
+    product_price: {
+        type: Number,
+        required: true,
+    },
+    product_promo_price: {
+        type: Number,
+        default: 0,
+    },
+    isPromo: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+productSchema.plugin(AutoIncrement, { inc_field: 'product_id' });
+const Product = mongoose.model('product', productSchema); // save product in db
+module.exports = Product;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(cors());
 const connectToDatabase = require('./db');
 const authModel = require('./models/userModel'); // import authModel
+const productModel = require('./models/productModel');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -13,18 +14,15 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000; // hosting port or local port
 
 app.get('/', async (req, res) => {
-    const data = await authModel.find({});
-    res.send({category: data});
-    console.log('jjjj');
-  });
-  app.post('/', async (req, res) => {
-    try {
-      const query = {userName: req.body.userName};
-      const data = await authModel.find(query);
-      res.send({category: data});
-    } catch (error) {
-      console.log(error);
-    }
+    var pipeline = [
+        { $lookup: { from: "items", localField: "product_id", foreignField: "product_id", as: "productbyItem" } },    ]
+    productModel.aggregate(pipeline)
+        .then((result) => {
+            res.status(200).json({ state: true, msg: "new product saved successfully!", data: result })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
   });
 
 
